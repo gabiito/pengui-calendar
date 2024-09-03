6207b97ec2c743cb0aa8e4c3dba10a46a1e4bcfc
Gabriel Zerbino <gabozerbino@gmail.com>
2024-09-03 08:19:02 -0300

    Refactor signal to be more explicit on methods

--- src/utils/signal.ts
+++ src/utils/signal.ts
@@ -5,29 +5,24 @@ export function createSignal<T>(initialValue: T) {
 
   let subs: { [K in keyof T]?: Set<Subscriber<T[K]>> } = {};
 
-  function signal<K extends keyof T>(key: K, newValue?: T[K]): T[K] | void {
-    // GETTER
-    if (newValue === undefined) {
+  return {
+    get: function<K extends keyof T>(key: K): T[K] {
       return value[key];
-    }
+    },
+    set: function<K extends keyof T>(key: K, newValue: T[K]) {
+      if (value[key] === newValue) {
+        return;
+      }
 
-    // SETTER
-    if (value[key] === newValue) {
-      return;
+      value = { ...value, [key]: newValue };
+      subs[key]?.forEach((sub) => sub(value[key]));
+    },
+    subscribe: function<K extends keyof T>(key: K, callback: Subscriber<T[K]>) {
+      if (!subs[key]) {
+        subs[key] = new Set();
+      }
+      
+      subs[key].add(callback);
     }
-
-    value = { ...value, [key]: newValue };
-    subs[key]?.forEach((sub) => sub(value[key]));
   }
-
-  signal.subscribe = function<K extends keyof T>(key: K, callback: Subscriber<T[K]>) {
-    if (!subs[key]) {
-      subs[key] = new Set();
-    }
-    
-    subs[key].add(callback);
-
-  }
-
-  return signal;
 }
